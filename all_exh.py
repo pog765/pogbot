191ebae1b69094c034af6933855c1513a291b510
pog765 <37500499+pog765@users.noreply.github.com>
2018-06-28 17:54:50 +0300

    Update all_exh.py

--- all_exh.py
+++ all_exh.py
@@ -17,3 +17,19 @@ multiplier = 1.1
 
 dg = api.getbalance(currency)
 print(dg)
+
+#https://support.bittrex.com/hc/en-us/articles/115003723911
+#покупка продажа
+
+#свершившиеся сделки
+b2-getorderhistory()
+p2=pl.hist(pair,cou)
+#открытые сделки 
+b3=api.getopenorders()
+
+#текушие цены
+b4=api.getticker()
+p4=pl.tick(par)
+#все свои монеты 
+b5=api.getbalances(currency)
+p5=pl.a1('returnBalances')
