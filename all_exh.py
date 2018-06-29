cbc604b10731179432451df9ca05671b3b480033
pog765 <37500499+pog765@users.noreply.github.com>
2018-06-29 14:24:15 +0300

    Update all_exh.py

--- all_exh.py
+++ all_exh.py
@@ -14,9 +14,10 @@ amount = 100
 # How big of a profit you want to make
 multiplier = 1.1
 
-
-dg = api.getbalance(currency)
-print(dg)
+def dg(currency):
+  dg = api.getbalance(currency)
+  print(dg)
+  return (dg)
 
 #https://support.bittrex.com/hc/en-us/articles/115003723911
 #покупка продажа
@@ -24,12 +25,24 @@ print(dg)
 #свершившиеся сделки
 b2-getorderhistory()
 p2=pl.hist(pair,cou)
-#открытые сделки 
-b3=api.getopenorders()
+
+
+#открытые сделки
+def opord(bir,pair):
+  if bir=='T':
+    b3=api.getopenorders(pair)
+    ext=b3
+    
+  elif bir=='P':
+    p3=pl.opord(pair)
+    ext=p3
+  print (ext)
+  return (ext)
 
 #текушие цены
 b4=api.getticker()
 p4=pl.tick(par)
+
 #все свои монеты 
 b5=api.getbalances(currency)
 p5=pl.a1('returnBalances')
